2be5ac8005e41cf319f7e9cf35a7daaba5b98236
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-07-14 21:57:06 +0200

    init commit

---
+++ platforms/android/assets/www/js/views/HomeView.js
@@ -0,0 +1,18 @@
+var HomeView = function () {
+
+    // this.initialize = function () {
+    //     // Define a div wrapper for the view (used to attach events)
+    //     this.$el = $('<div/>');      
+    //     // this.render();
+    // };    
+
+    // this.render = function() {
+    //     // this.$el.html(this.template()); 
+    //     // $('.content', this.$el).html();
+    //     return this;
+    // }
+
+    // this.initialize();
+    // this.render();
+    return $('<div>').html(this.template()); 
+}
---
+++ www/js/app.js
@@ -0,0 +1,64 @@
+(function () {
+  // // default route
+  // router.addRoute('', function() {
+  //   displayTemplate('homeTemplate');
+  // });
+
+  // // others routes
+  // var path = ['home','faq'];
+  // path.forEach(function(entry) {
+  //     router.addRoute(entry, function() {
+  //       displayTemplate(entry + 'Template');
+  //     });
+  // });
+
+  // router.start();
+  
+  var slider = new PageSlider($('#tmplContent'));
+  //routes determined by hash
+  $(window).on('hashchange', function(){
+    var hash = window.location.hash.substr(1);
+    var activeMenu = hash + 'Active';
+    loadMenu(activeMenu);
+    slider.slidePage($('<div>').html(loadTemplate(hash)));
+  });
+
+  //default page when no hash
+  // $('#tmplContent').load('js/templates/homeTemplate.html');
+  $('#tmplContent').html(loadTemplate('home'));
+  loadMenu('homeActive');
+  
+  /**
+   * call the menu template and set the active menu (with handlebars.js)
+   * @param  {activeMenu [string]}
+   */
+  function loadMenu(activeMenu)
+  {
+    var loaded = loadTemplate('menu');
+    var obj = {};
+    obj[activeMenu] = 'active';
+    var data=[];
+    data.push(obj);
+    $('#menuContent').html(loaded(data[0]));
+  }
+
+  /**
+   * get a template, compile it with handblebars.js and return his html content
+   * @param  {tmpl_name [string]}
+   * @return {htmlContent [handlebars compiled function]}
+   */
+  function loadTemplate(tmpl_name) {
+    var tmpl_url = 'js/templates/' + tmpl_name + 'Template.html';
+    var htmlContent;
+    $.ajax({
+        url: tmpl_url,
+        method: 'GET',
+        dataType: 'html',
+        async: false,
+        success: function(data) {
+            htmlContent = Handlebars.compile(data);
+        } 
+    });
+    return htmlContent;
+  }
+}());
