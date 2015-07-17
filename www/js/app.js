5a538d0918da2e51098b5723767c107c3ce1c8a0
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-07-18 01:44:46 +0200

    gestion menu et contenu pages avec handlebars pour traduction avec plusieurs fichiers

--- www/js/app.js
+++ www/js/app.js
@@ -1,72 +1,58 @@
 (function () {
-  // // default route
-  // router.addRoute('', function() {
-  //   displayTemplate('homeTemplate');
-  // });
-
-  // // others routes
-  // var path = ['home','faq'];
-  // path.forEach(function(entry) {
-  //     router.addRoute(entry, function() {
-  //       displayTemplate(entry + 'Template');
-  //     });
-  // });
-
-  // router.start();
-  
+  var slider = new PageSlider($('#tmplContent'))
   // browser language detection and load corresponding language file 
   var lang = window.navigator.userLanguage || window.navigator.language
   lang = lang.substr(0,2)
   $('body').append('<script src="assets/lang/' + lang + '.js"></script>')
   
-  var slider = new PageSlider($('#tmplContent'))
   //routes determined by hash
   $(window).on('hashchange', function(){
     var hash = window.location.hash.substr(1)
     var activeMenu = hash + 'Active'
-    loadMenu(activeMenu)
-    slider.slidePage($('<div>').html(loadTemplate(hash)))
+    loadTemplate('menu', activeMenu)
+    loadTemplate(hash)
   });
 
   //default page when no hash
-  slider.slidePage($('<div>').html(loadTemplate('home')))
-  loadMenu('homeActive')
+  loadTemplate('menu', 'homeActive') //default menu
+  loadTemplate('home') //homepage
   
-  /**
-   * call the menu template and set the active menu (with handlebars.js)
-   * @param  {activeMenu [string]}
-   */
-  function loadMenu(activeMenu)
-  {
-    var loaded = loadTemplate('menu')
-    var obj = {}
-    obj[activeMenu] = 'active'
-    obj['homeTitle'] = homeTitle
-    obj['faqTitle'] = faqTitle
-    obj['contactTitle'] = contactTitle
-    var data=[]
-    data.push(obj)
-    $('#menuContent').html(loaded(data[0]))
-  }
 
   /**
    * get a template, compile it with handblebars.js and return his html content
    * @param  {tmpl_name [string]}
    * @return {htmlContent [handlebars compiled function]}
    */
-  function loadTemplate(tmpl_name) {
+  function loadTemplate(tmpl_name, activeMenu) {
     var tmpl_url = 'js/templates/' + tmpl_name + 'Template.html'
     var htmlContent
+    var context = {}
+
     $.ajax({
         url: tmpl_url,
         method: 'GET',
-        data : { homeSubtitle : homeSubtitle },
         dataType: 'html',
         async: false,
         success: function(data) {
-            htmlContent = Handlebars.compile(data)
+          // find Handlebars expressions in nude template (before compilation)
+          var i=0          
+          var tab = data.split("{{")
+          $.each(tab, function(key, value){
+            if(i > 0) {
+              var tab2 = value.split("}}")
+              var expr = tab2[0]
+              // build context for future Handlebars compilation
+              context[expr] = eval(expr)
+            }
+            i++
+          })
+          if(activeMenu != null) { context[activeMenu] = 'active' } // case of menu loading 
+          // compile template
+          var template = Handlebars.compile(data)
+          var html = template(context)
+          if(activeMenu != null) { $('#menuContent').html(html) } // case of menu loading
+          else { slider.slidePage($('<div>').html(html)) }
         } 
     });
-    return htmlContent
   }
 }());
