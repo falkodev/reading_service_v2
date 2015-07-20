852bc66aa34e8ca2b0f675356be65a1806955d3b
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-07-20 19:07:06 +0200

    ajout font-awesome + page contact

--- www/assets/lang/en.js
+++ www/assets/lang/en.js
@@ -1,10 +1,34 @@
-homeTitle = "Home"
-faqTitle = "FAQ"
-contactTitle = "Contact"
-homeActive = ""
-faqActive = ""
-contactActive = ""
+menu_home = "Home"
+menu_faq = "FAQ"
+menu_contact = "Contact"
+home_active = ""
+faq_active = ""
+contact_active = ""
 homeSubtitle = "Subscribe or login"
 homeFAQLabel = "Tell me more"
 homeSubscribeLabel = "Create an account"
 homeLoginLabel = "Login"
+faq_title = "Questions à propos de ce site"
+faq_question_1_title = 'Pourquoi ce site ?'
+faq_question_1_content = "Pour que tout le monde puisse en profiter. Ce site n'est bien sûr pas lié à jw.org ou à quelque entité associée aux Témoins de Jéhovah. Il ne met à disposition que des portions de la Bible et n'a pas pour but d'enseigner ou de propager des points de vue doctrinaux. {{{<br>}}}{{{<br>}}}Le service délivré à travers ce site est gratuit et n'a aucune vocation commerciale. De ce fait, il est mis à disposition de tous, et ne sera jamais supporté par de la publicité."
+faq_question_2_title = 'Qui est l\'auteur du site ?'
+faq_question_2_content = "En plus d'être Témoin de Jéhovah, je suis informaticien et surtout passionné par le développement. J'ai moi-même souvent oublié de terminer ma lecture hebdomadaire, ou de faire les recherches nécessaires. Ou bien, j'ai été obligé de lire de grosses portions d'un coup pour rattraper du retard. J'ai donc conjugué ma passion pour le développement et mes besoins théocratiques pour créer un service web m'envoyant un email chaque matin avec la portion du jour. {{{<br>}}}{{{<br>}}}De ce service sommaire, j'ai voulu mettre à disposition de tous ce que je pensais être utile. N'hésitez pas à m'indiquer ce que vous en pensez, que vous trouviez que ce soit une bonne idée ou pas, ce qu'il faudrait améliorer...Tant que la critique est constructive, elle sera appréciée :)"
+faq_question_3_title = 'Quel est ce site ? Comment ça marche ?'
+faq_question_3_content = "Ce site permet de {{{<a href='index.html#subscribe'>}}}créer un compte{{{</a>}}} pour recevoir des portions de la lecture de la Bible. Vous pouvez choisir les jours de réception, ainsi que le texte du jour si vous le désirez. {{{<br>}}}{{{<br>}}}Le programme hebdomadaire et la traduction de la Bible utilisés sont issus de jw.org."
+faq_question_4_title = 'Que faire en cas de mot de passe perdu ?'
+faq_question_4_content = "{{{<a href='index.html#login'>}}}Dans le menu de connexion{{{</a>}}}, un lien \"Mot de passe oublié\" est présent. Après avoir entré l'adresse mail utilisée pendant votre inscription et cliqué sur le bouton de validation, un nouveau mot de passe sera généré et expédié sur votre adresse email. {{{<br>}}}{{{<br>}}}En cas de problème, vous pouvez toujours envoyer un message à l'administrateur JW Reading {{{<a href='index.html#contact'>}}}avec le formulaire de contact présent sur le site.{{{</a>}}}"
+faq_question_5_title = 'Comment me désinscrire ?'
+faq_question_5_content = "Si vous désirez supprimer votre compte JW Reading, vous pouvez cliquer dans le lien de désinscription présent en bas de chaque email que vous recevez de jwreading.com. {{{<br>}}}{{{<br>}}}Si vous voulez simplement désactiver votre compte sans le supprimer, veuillez envoyer un message à l'administrateur JW Reading {{{<a href='index.html#contact'>}}}avec le formulaire de contact présent sur le site.{{{</a>}}}"
+faq_question_6_title = 'Comment montrer que j\'apprécie le site ?'
+faq_question_6_content = "Si vous pensez que ce site est une bonne idée, vous pouvez toujours en remercier l'auteur {{{<a href='index.html#contact'>}}}avec un petit message d'encouragement ;){{{</a>}}}{{{<br>}}}{{{<br>}}} Vous pouvez aussi parler de ce site autour de vous. Ce sera le moyen de le faire connaître et de me pousser à l'améliorer."
+faq_question_7_title = 'Je vois la portion du jour dans mon email quotidien. Pourquoi un lien vers la version online cette portion est-il inclus ?'
+faq_question_7_content = "Déjà, en cas de problème d'affichage, vous pouvez voir directement la portion sur le site.{{{<br>}}}{{{<br>}}}Mais surtout, les versets qui sont surlignés mènent à des recherches de publications situées en bas de la portion. Ce sont des liens spéciaux appelés \"ancres\". Or, les clients emails actuels bloquent les liens de type ancre pour des raisons de sécurité. Les ancres permettent d'aller à la recherche et de remonter au verset par de simples clics sans avoir à descendre et remonter manuellement. Pour profiter de cette fonctionnalité, il faut passer par la version online."
+faq_legal_title = 'Mentions légales'
+faq_legal_content = 'Hébergeur : {{{<a href="http://www.ovh.com/fr/support/documents_legaux/" target="_blank">}}}OVH{{{</a>}}}{{{<br>}}}Siège social : 2 rue Kellermann 59100 Roubaix - France{{{<br>}}}Tél : 0 820 698 765'
+contact_title = 'Formulaire de contact'
+contact_name = 'Votre nom'
+contact_email = 'Votre adresse email'
+contact_msg = 'Votre message'
+contact_btn_send = 'Envoyer'
+contact_thanks_msg = "Merci pour votre message. Il a bien été envoyé. Vous aurez une réponse d'ici peu."
+btn_back = 'Retour'
