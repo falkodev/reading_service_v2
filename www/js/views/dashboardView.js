b2146cca25fcfc2d141598fd20180d1b53f82c5e
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-08-03 21:21:02 +0200

    daily text ok

--- www/js/views/dashboardView.js
+++ www/js/views/dashboardView.js
@@ -1,1 +1,8 @@
-var dashboardView = function () {}
+var dashboardView = function () {
+	$.post("http://www.jwreading.com/ajax/getDailyText.php", 
+	   { 'lang': lang }, function(data) {}).complete(function(data) {
+	result = data['responseText'].trim();
+	// console.log('result: ' + result);
+	$('#dailyText').html(result);
+});
+}
