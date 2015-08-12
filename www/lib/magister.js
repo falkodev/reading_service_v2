eaa59ac7fbf9ffaf78febf0131d81f45afa47037
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-08-12 23:58:38 +0200

    menu glissant sur petit écran ok, a faire : menu visible/invisible sur grand ecran comme avant

--- www/lib/magister.js
+++ www/lib/magister.js
@@ -19,45 +19,27 @@ jQuery(document).ready(function($) {
     });
 
     $(document).on( "click", ".navbar-toggle", function(){
-    	var l = $('#tmplContent').css("left");
     	var w = $(window).width();
-    	// console.log('l: ' + l + ' w: ' + w);
     	if(w < 768) {    		    		
-    		if(l == "auto"|| l == "0px") {
-	    		$('#tmplContent').css({"position":"relative", "left":"180px", "top":"-78px"});
-	    		$('.navbar-toggle').css("left","160px");
-	    		// console.log('left à 160 px');
+    		if(!$('#tmplContent').hasClass("menu-open")) {
+    			$('#tmplContent').addClass("menu-open");
 	    	}
 	    	else {
-	    		$('#tmplContent').css({"position":"relative", "left":"0px", "top":"-78px"});
-	    		$('.navbar-toggle').css("left","0px");
-	    		// console.log('left à 0 px');
+	    		$('#tmplContent').removeClass("menu-open");
 	    	}
     	}
-    	else {
-			$('#tmplContent').css({"position":"static", "left":"auto", "top":"auto"});
-		}
-		$('.mainmenu').css({"padding":"0px", "top":"-4px"});
-		$('.dropdown-menu').css({"padding":" 30px 0 0 0"});
-		// $('.navbar-toggle').css("margin"," 41px 0 0 0");
-		// var d = $('.dropdown-menu').css("left");
-		// console.log("dropdown-menu: " + d);
+  //   	else {
+		// 	$('#tmplContent').css({"position":"static", "left":"auto", "top":"auto"});
+		// }
     });
 
     $(document).on( "click", function(){
     	var w = $(window).width();
     	if(w < 768) { 
-	    	$('#tmplContent').css({"position":"relative", "left":"0px", "top":"-78px"});
-	    	$('.navbar-toggle').css("left","0px");
-			// console.log('left à 0 px');
+    		$('#tmplContent').removeClass("menu-open");
 		}
-		else {
-			$('#tmplContent').css({"position":"static", "left":"auto", "top":"auto"});
-		}
-		// var d = $('.dropdown-menu').css("left");
-		// console.log("dropdown-menu: " + d);
-		$('.mainmenu').css("padding","30px 0 0 0");
-		$('.dropdown-menu').css({"padding":" 30px 0 0 0"});
-		// $('.navbar-toggle').css("margin"," 0px 0 0 0");
+		// else {
+		// 	$('#tmplContent').css({"position":"static", "left":"auto", "top":"auto"});
+		// }
     });
 });
