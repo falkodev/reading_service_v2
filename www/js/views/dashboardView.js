4ae3166c85e497b9e52fe499de329db4b0a70e04
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-09-25 23:11:21 +0200

    Lecture du jour : début

--- www/js/views/dashboardView.js
+++ www/js/views/dashboardView.js
@@ -1,11 +1,12 @@
 var dashboardView = function () {
-	
+	//get daily text
 	$.post("http://www.jwreading.com/ajax/getDailyText.php", 
 	   { 'lang': lang }, function(data) {}).complete(function(data) {
 		result = data['responseText'].trim();
 		$('#dailyText').html(result);
 	});
 
+	//get rss feed
     $.ajax({
         type: "GET",
         url: "http://www.jwreading.com/ajax/rss.php",
@@ -19,7 +20,6 @@ var dashboardView = function () {
         	var slider2 = new PageSlider($('#feed-rotator2')); //slide for right feed-rotator
 
         	function rotateNews(newsNumber, feedRotatorNumber) {
-
 				var display = $('#news' + newsNumber, html); //get specific news to display
 				$.each(display, function(key, value){
 		    		$.each(value, function(k, v){
@@ -31,15 +31,18 @@ var dashboardView = function () {
 		    			}
 		    		});
 		    	}); 
-			}        
-			
-        	var interval = window.setInterval(function() {
-        		if(y==0) { //first time 
+			}   
+
+			var interval = window.setInterval(function() {
+				if(y==0) { //first time 
         			$('#waitDiv').hide(); //when loading is done, hide the loading animation
         			y = 2; //feeding right feed rotator
         			rotateNews(x, y); 
     			} 
-        		else {
+			}, 1000); // load animation during 1 second (enough to load rss feed)		     
+			
+        	var interval = window.setInterval(function() {
+        		if(y == 1 || y == 2) {
         			y = 1;
 					rotateNews(x, y); //feeding left feed rotator
 
@@ -48,7 +51,29 @@ var dashboardView = function () {
 					y = 2;
 					rotateNews(x, y); //feeding right feed rotator
 				}					
-			}, 5000); // display another news every 5 seconds		
+			}, 5000); // display every news during 5 seconds		
         }
     });	
+
+	//determine if user has a portion today, what portion it is and enable button to access the portion of the day if necessary
+	var sessionUserData = JSON.parse(sessionStorage.getItem("sessionUserData")); // retrieve user data
+	var today = new Date().getDay(); // day of the week for today (Monday = 1, Tuesday = 2, ...)
+	var day = 'sessionUserData.day' + today;
+	var dayConfig = eval(day); // today in the user config
+	if(dayConfig) {
+		var todayDate = new Date();
+		// console.log("date " + isDate(todayDate));
+		var currentWeek = getWeek(todayDate);
+		console.log('week ' + currentWeek);	
+
+		//déterminer ici quel "jour" on est par rapport au 1er jour de lecture (reprendre logique PHP) puis déterminer quelle semaine prendre (l'actuelle ou la suivante) + gérer derniere semaine de l'année
+		//lire dans la portion du jour (fichier html sur serveur) : déterminer que la portion va de "xx:xx à yy:yy"
+	}
+
+ 	function getWeek(day) {
+		var onejan = new Date(day.getFullYear(),0,1);
+        var today = new Date(day.getFullYear(),day.getMonth(),day.getDate());
+        var dayOfYear = ((today - onejan +1)/86400000);
+        return Math.ceil(dayOfYear/7);
+	}
 }
