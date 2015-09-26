e80ffd9a3d09d4cbcee53a1b85e809805837cb7c
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-09-27 00:47:34 +0200

    lecture du jour : chargement

--- www/js/views/dashboardView.js
+++ www/js/views/dashboardView.js
@@ -58,16 +58,69 @@ var dashboardView = function () {
 	//determine if user has a portion today, what portion it is and enable button to access the portion of the day if necessary
 	var sessionUserData = JSON.parse(sessionStorage.getItem("sessionUserData")); // retrieve user data
 	var today = new Date().getDay(); // day of the week for today (Monday = 1, Tuesday = 2, ...)
+	today++; //otherwise Sunday = 0
 	var day = 'sessionUserData.day' + today;
 	var dayConfig = eval(day); // today in the user config
 	if(dayConfig) {
-		var todayDate = new Date();
-		// console.log("date " + isDate(todayDate));
-		var currentWeek = getWeek(todayDate);
-		console.log('week ' + currentWeek);	
-
 		//déterminer ici quel "jour" on est par rapport au 1er jour de lecture (reprendre logique PHP) puis déterminer quelle semaine prendre (l'actuelle ou la suivante) + gérer derniere semaine de l'année
 		//lire dans la portion du jour (fichier html sur serveur) : déterminer que la portion va de "xx:xx à yy:yy"
+		var nbJours = 0; 
+		var beforeFirstCycleDay = 0;
+		var numeroPortion=0;
+		if(today == sessionUserData.firstDay) { numeroPortion = 1; }
+		else {
+			for(var i=1;i<8;i++)
+			{
+				day = 'sessionUserData.day' + i;
+				dayConfig = eval(day); 
+				if(dayConfig == 1) 
+				{
+					nbJours++;
+					if(i < sessionUserData.firstDay) { beforeFirstCycleDay = 1; }
+					
+					//2 cas de figure : 
+					//1er cas :  si le jour actuel est plus grand que le "premier" jour du cycle, il faut prendre en compte le jour actuel + les jours à la fois plus grands que le "premier" jour et plus petits que le jour actuel
+					if(today > sessionUserData.firstDay && i <= today && i >= sessionUserData.firstDay) { numeroPortion++; }
+					//2e cas : si le jour actuel est plus petit que le "premier" jour du cycle, il faut prendre en compte le jour actuel + les jours plus petits que le jour actuel + les jours plus grands que le "premier" jour + le "premier" jour
+					else if(today < sessionUserData.firstDay && ( (i <= today) || (i >= sessionUserData.firstDay)) ) { numeroPortion++; }
+				}
+			}
+		}
+		// console.log('numeroPortion:' + numeroPortion);
+		var semaineProchaine = 0;
+		if(today >= sessionUserData.firstDay && beforeFirstCycleDay == 1) { semaineProchaine = 1; }
+		
+		var todayDate = new Date();
+		var currentWeek = getWeek(todayDate);
+
+		if(semaineProchaine == 1) { 
+			if(currentWeek == 53) { currentWeek = "01"; }
+			else { 
+				currentWeek++; 
+				if(currentWeek < 10) { currentWeek = "0" + currentWeek; }
+			}
+		}
+		// console.log('week ' + currentWeek);	
+
+		var file = 	nbJours + "" + numeroPortion;
+		$.ajax({
+	        type: "GET",
+	        url: "http://www.jwreading.com/ajax/getContents.php",
+	        dataType: 'html',
+	        data: {
+	        	'lang' : sessionUserData.readingLang,
+	        	'week' : currentWeek,
+	        	'file' : file },        
+	        success: function(html){ 
+	        	console.log(html);  
+	        	data = JSON.parse(html); 
+	        	$('#lecture').html(data['content']);
+	        	var idFrom = $('input[value="' + data['from'] + '"]').parent().css("color", "red").attr('id'); 
+	        	var idTo = $('input[value="' + data['to'] + '"]').parent().css("color", "red").attr('id'); 
+	        	$('#' + idFrom).nextUntil('#' + idTo).css("color", "red");  
+	        	location.hash = '#' + idFrom;
+	        }
+	    });	
 	}
 
  	function getWeek(day) {
