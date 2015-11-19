842532163e9dd1785fc3235a04a1c737f337c509
Anthony Tarlao <anthony.tarlao@gmail.com>
2015-11-19 22:50:26 +0100

    extraction code pour savoir si portion du jour dans app.js

--- www/js/views/todayReadingView.js
+++ www/js/views/todayReadingView.js
@@ -3,48 +3,7 @@ var todayReadingView = function () {
 		$('#todayPortionValidate').hide();
 		var sessionUserData = JSON.parse(localStorage.getItem("sessionUserData")); // retrieve user data
 		var today = new Date().getDay(); // day of the week for today (Monday = 1, Tuesday = 2, ...)
-
-		if(today == 0) { today = 7; }//otherwise Sunday = 0
-		var day;
-		var dayConfig;
-		var totalDays = 0; 
-		var beforeFirstCycleDay = 0;
-		var portionNumber=0;
-		
-		//find what "day" it is compared to the first reading day
-		for(var i=1;i<8;i++)
-		{
-			day = 'sessionUserData.day' + i;
-			dayConfig = eval(day); 
-
-			if(dayConfig == 1) 
-			{
-				totalDays++;
-				if(i < sessionUserData.firstDay) { beforeFirstCycleDay = 1; }
-				
-				//2 possibles cases : 
-				//1st case :  if current day is greater than the "first" day of the cycle, count current day + days greater than the "first" day + days smaller than the current day
-				if(today > sessionUserData.firstDay && i <= today && i >= sessionUserData.firstDay) { portionNumber++; }
-				//2nd case : if current day is smaller than the "first" day of the cycle, count current day + days smaller than the current day + days greater than the "first" day + "first" day
-				else if(today < sessionUserData.firstDay && ( (i <= today) || (i >= sessionUserData.firstDay)) ) { portionNumber++; }
-			}
-		}
-		if(today == sessionUserData.firstDay) { portionNumber = 1; }
-		var nextWeek = 0;
-		if(today >= sessionUserData.firstDay && beforeFirstCycleDay == 1) { nextWeek = 1; } //find what week (current or next one) to look for
-
-		var todayDate = new Date();
-		var currentWeek = getWeekNumber(todayDate); //find current week number
-
-		if(nextWeek == 1) { 
-			if(currentWeek == 53) { currentWeek = "01"; }
-			else { 
-				currentWeek++; 
-				if(currentWeek < 10) { currentWeek = "0" + currentWeek; }
-			}
-		}
-
-		var file = 	totalDays + "" + portionNumber;
+		var data = checkIfPortion();
 		/**
 		 * [get weekly reading and display today's portion if applicable]
 		 */
@@ -61,10 +20,7 @@ var todayReadingView = function () {
 		        200); // if ajax request takes more than 200ms, display loading animation         	
             },
             timeout: 10000,
-	        data: {
-	        	'lang' : sessionUserData.readingLang,
-	        	'week' : currentWeek,
-	        	'file' : file },        
+	        data: data,         
 	        success: function(html){  
 	        	data = JSON.parse(html); // data contains informations about the first and last verses of today's portion and the whole content of the weekly reading
 	        	$('#todayPortionFrom').html(data['from']);
@@ -76,12 +32,11 @@ var todayReadingView = function () {
 	        	if(dayConfig == 1) { //if today is a reading day for the user
 	        		var len = $('span.anchor').length; 
 	        		//color in pure white every verse of the portion and in orange every verse number pointing to an anchor (hence a biblical research)
-					var idFrom = $('input[value="' + data['from'] + '"]').parent().css({"color":"#fff","font-weight":"500" }).attr('id'); 
-		        	$('input[value="' + data['from'] + '"]').prev('a').css({"color":"#FB7900","font-weight":"500" }); 
+					var idFrom = $('input[value="' + data['from'] + '"]').parent().css({"color":"#fff","font-weight":"500" }).attr('id');  
 		        	var idTo = $('input[value="' + data['to'] + '"]').parent().css({"color":"#fff","font-weight":"500" }).attr('id'); 
-		        	$('input[value="' + data['to'] + '"]').prev('a').css({"color":"#FB7900","font-weight":"500" }); 
-		        	$('#' + idFrom).nextUntil('#' + idTo).css({"color":"#fff","font-weight":"500" }).find('a').css({"color":"#FB7900","font-weight":"500" }); 
-		        	
+		        	$('#' + idFrom).nextUntil('#' + idTo).css({"color":"#fff","font-weight":"500" }); 
+		        	$('input').prev('a').css({"color":"#FB7900","font-weight":"500" }); 
+
 		        	if(idFrom != 1) { //if not the first portion of the week, display the "Display previous verses" button and blur previous verse
 		        		var beforeFrom = parseInt(idFrom) - 1;
 		        		$('#1').nextUntil('#' + beforeFrom).andSelf().hide(); 
@@ -181,20 +136,4 @@ var todayReadingView = function () {
 	/**
 	 * [End zoom in / zoom out mgmt]
 	 */
-
-	// For a given date, get the ISO week number
-    function getWeekNumber(d) {
-	    // Copy date so don't modify original
-	    d = new Date(+d);
-	    d.setHours(0,0,0);
-	    // Set to nearest Thursday: current date + 4 - current day number
-	    // Make Sunday's day number 7
-	    d.setDate(d.getDate() + 4 - (d.getDay()||7));
-	    // Get first day of year
-	    var yearStart = new Date(d.getFullYear(),0,1);
-	    // Calculate full weeks to nearest Thursday
-	    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
-	    // Return week number
-	    return weekNo;
-	}
 }
